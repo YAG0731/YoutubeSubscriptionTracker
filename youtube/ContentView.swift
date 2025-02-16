9b563cfa7f00ba749d9b18d23bd45c0167bc788b
Yunao Guo <yunao.guo@sjsu.edu>
2025-02-15 21:31:50 -0800

    Initial Commit

---
+++ youtube/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  youtube
+//
+//  Created by Yunao Guo on 2/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ youtube/youtubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtubeApp.swift
+//  youtube
+//
+//  Created by Yunao Guo on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct youtubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
